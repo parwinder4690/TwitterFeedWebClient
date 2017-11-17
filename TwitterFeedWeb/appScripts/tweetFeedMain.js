95696f111cff51dc032116100b907f6fe3be7afc
Parwinder Kaur <parwinder4690@gmail.com>
2017-11-17 00:32:35 -0500

    Added comments and get out the setInterval function out.

--- TwitterFeedWeb/appScripts/tweetFeedMain.js
+++ TwitterFeedWeb/appScripts/tweetFeedMain.js
@@ -3,6 +3,6 @@ $(document).ready(function () {
     var tweetControl = $("#divTweets").twitterFeed();
 
     $("#txtSearchText").on('change keyup copy paste cut', function () {
-        $("#divTweets").tweet("setFilter", $('#txtSearchText').val());
+        $("#divTweets").twitterFeed("setFilter", $('#txtSearchText').val());
     });
 });
--- TwitterFeedWeb/appScripts/twitterFeed.js
+++ TwitterFeedWeb/appScripts/twitterFeed.js
@@ -1,8 +1,6 @@
-﻿console.log("loding widget file...");
-
-
+﻿
+// twitterFeed widget hanlde access to api and filter the tweets
 $(document).ready(function () {
-    console.log("Load Widget");
         $.widget("ui.twitterFeed", {
             options: {
                 data: [],
@@ -13,51 +11,26 @@ $(document).ready(function () {
             {
                 var self = this;
 
-                var strVar = "";
-                strVar += "<div class=\"row feedContent\" style=\"margin-bottom:10px;\">";
-                strVar += "        <div class=\"col-md-2\" style=\"float:left;width:58px;margin-bottom:10px;\">";
-                strVar += "            <img src=\"${ProfileImageUrl}\" style=\"width:48px;height:48px; border-radius:50%;\" \/>";
-                strVar += "        <\/div>";
-                strVar += "        <div class=\"col-md-4\" style=\"float:left;width:90%;\">";
-                strVar += "";
-                strVar += "            <div stye=\"width:100%;float:left;\">";
-                strVar += "                <div style=\"float:left;font-weight:bold;\">${UserName}<\/div>";
-                strVar += "                <div style=\"margin-left:10px;\"><span style=\"margin-left:20px\">${ScreenName}<\/span><\/div>";
-                strVar += "            <\/div>";
-                strVar += "";
-                strVar += "            <div class=\"tweetText\" stye=\"width:100%;float:left;\">${Text}<\/div>";
-                strVar += "";
-                strVar += "            <div class=\"tweetfooter\" stye=\"width:100%;\">";
-                strVar += "                <div style=\"float:left;\"><span class=\"glyphicon glyphicon-calendar\"><\/span> ${CreatedAtxt}<\/div>";
-                strVar += "                <div style=\"float:left;\"><span style=\"margin-left:20px\"><span class=\"glyphicon glyphicon-retweet\"><\/span> ${RetweetCount}<\/span><\/div>";
-                strVar += "            <\/div>";
-                strVar += "        <\/div>";
-                strVar += "    <\/div>";
+                var urlTemplate = "/appScripts/feedContent.html";
+                $.when($.get(urlTemplate)).then(function (_html) {
+                    self.options.template = _html;
 
-                self.options.template = strVar;
-                //var urlTemplate = "/appScripts/feedContent.html";
-                //$.when($.get(urlTemplate)).then(function (_html) {
-                //    self.options.template = _html;
-
-                //    self.load();
-
-                //    setInterval(function () {
-                //        self.load();
-                //    }, 10000);
-
-                //});
-
-                self.load();
-
-                setInterval(function () {
                     self.load();
-                }, 10000);
-               
+                    self.setInterval();
+                });
+ 
+            },
+            setInterval: function (_self)
+            {
+                setInterval(function () {
+                    _self.load();
+                }, 1000 * 60 );
             },
             load: function ()
             {
                 var self = this;
-                var url = "http://localhost:61989/api/TimeLineFeed";
+                //var url = "http://localhost:61989/api/TimeLineFeed";
+                var url ="http://35.184.238.128/TwitterFeedAPI/api/TimeLineFeed"
                 $.when($.getJSON(url)).then(function (_data) {
                     self.options.data = _data;
                     self.renderTweets();
@@ -65,11 +38,10 @@ $(document).ready(function () {
             },
             renderTweets: function ()
             {
-                console.log("renderTweets...");
                 var self = this;
                 var el = self.element;
                 var data = self.options.data;
-
+                //Set filter if any filter text in filterText 
                 if (self.options.filterText != '')
                 {
                     data = data.filter(function (tweetData, index) {
@@ -78,7 +50,6 @@ $(document).ready(function () {
                 }
                 var divContent = document.createElement("div");
                 data.forEach(function (value, index) {
-                    //$("#templateContent").tmpl(value).appendTo(divContent);
                     $(self.options.template).tmpl(value).appendTo(divContent);
                 });
    
